001b5cae900ef8b88f30b05da1e1a95bd7f57205
aierlma <aierlma521@gmail.com>
2020-10-07 19:06:38 +0800

    first commit

---
+++ 搜索字幕.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  2 14:25:35 2020
+
+@author: Aierlma
+"""
+
+import requests
+import os
+import re
+
+x = os.getcwd()
+files = os.listdir(x)#取得本目录下所有文件，生成一个列表
+print(files)
+file1 = open(x+"\\temp.txt","w")
+count = 0
+for i in files:
+    if '-C' in i or '-c' in i:
+        continue
+    if "[" in i:
+        i = re.sub('\\[.*?\\]', '', i)
+    
+    fanhao = os.path.splitext(i)[0]
+    print(fanhao)
+    c = 'https://www.dmmbus.cam/search/%s&type=&parent=ce'%fanhao
+    a = requests.get(c) 
+    print(c)
+    b = a.text
+    print("包含字幕的磁力連結" in b)
+    if "包含字幕的磁力連結" in b:
+        count += 1
+        file1.write('https://www.dmmbus.cam/search/%s&type=&parent=ce'%fanhao + "\n")
+        
+file1.close()        
+print(count)
---
+++ 文字识别.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 15:35:40 2020
+
+@author: QGX
+"""
+
+from aip import AipOcr
+
+import re
+import os
+import time
+
+""" 你的 APPID AK SK """
+APP_ID = ''
+API_KEY = ''
+SECRET_KEY = ''
+
+client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+
+path = os.getcwd()
+listimg = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".jpeg")]
+
+for foo in range(0,260):
+    a = 2*foo
+    for _ in range(a,a+2):
+        file = listimg[_]
+        i = open(file, 'rb').read()
+        #result = client.basicGeneral(i)#通用
+        result = client.basicAccurate(i)#精准
+        print(result)
+
+        with open("result.txt", "a", encoding='utf-8') as resfile:
+            while "words_result" not in result:
+                result = client.basicAccurate(i)
+            for item in result['words_result']:
+                resfile.write(item['words'])
+                resfile.write('\n')
+                print(file)
+    time.sleep(1)
